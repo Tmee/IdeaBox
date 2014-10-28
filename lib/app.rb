85fdbac53e47e69ec73db16c9b438f51b6151179
Timothy Mee <tjmee90@gmail.com>
2014-10-28 15:37:34 -0600

    tutorial done

--- lib/app.rb
+++ lib/app.rb
@@ -11,11 +11,19 @@ class IdeaBoxApp < Sinatra::Base
   set :root, 'lib/app'
 
   get '/' do
-    erb :index, locals: {ideas: IdeaStore.all, idea: Idea.new(params)}
+    erb :index, locals: {ideas: IdeaStore.all.sort, idea: Idea.new(params)}
   end
 
   post '/' do
     IdeaStore.create(params[:idea])
+    redirect '/'
+  end
+
+  post '/:id/like' do |id|
+    idea = IdeaStore.find(id.to_i)
+    idea.like!
+    IdeaStore.update(id.to_i, idea.to_h)
+    "I like this idea!"
     redirect '/'
   end
 
@@ -26,7 +34,7 @@ class IdeaBoxApp < Sinatra::Base
 
   get '/:id/edit' do |id|
     idea = IdeaStore.find(id.to_i)
-    erb :edit, locals: {id: id, idea: idea}
+    erb :edit, locals: {idea: idea}
   end
 
   put '/:id' do |id|
--- lib/idea_box/idea.rb
+++ lib/idea_box/idea.rb
@@ -1,18 +1,34 @@
 require 'yaml/store'
 
 class Idea
-  attr_reader :title, :description
+  include Comparable
+  attr_reader :title, :description, :rank, :id
 
-  def initialize(attributes)
+  def initialize(attributes = {})
     @title       = attributes["title"]
     @description = attributes["description"]
+    @rank        = attributes["rank"] || 0
+    @id          = attributes["id"]
   end
 
   def save
-    database.transaction do |db|
-      db['ideas'] ||= []
-      db['ideas'] << {"title" => title, "description" => description}
-    end
+    IdeaStore.create(to_h)
+  end
+
+  def to_h
+  {
+    "title" => title,
+    "description" => description,
+    "rank" => rank
+  }
+  end
+
+  def like!
+    @rank += 1
+  end
+
+  def <=>(other)
+    other.rank <=> rank
   end
 
   def database
