e86fb54e68d338e1b81177f5dbe2d7cae56f2be4
Josh Cheek <josh.cheek@gmail.com>
2014-10-30 15:59:18 -0600

    Implement first test, consider future tests

--- test/app_test.rb
+++ test/app_test.rb
@@ -4,11 +4,39 @@ Bundler.require
 require 'app'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'nokogiri'
 
 class IdeaBoxAppTest < Minitest::Test
   include Rack::Test::Methods
 
-  def test_it_failse
-    assert false
+  def setup
+    IdeaStore.salt_the_earth
+  end
+
+  def app
+    IdeaBoxApp
+  end
+
+  def test_index_lists_all_the_ideas_sorted_by_how_liked_they_are
+    # put three ideas in the database, liked in a way that requires sorting
+    IdeaStore.create 'title' => 'a', 'rank' => 3
+    IdeaStore.create 'title' => 'b', 'rank' => 1
+    IdeaStore.create 'title' => 'c', 'rank' => 2
+
+    # go to the index
+    get '/'
+
+    # assert that I see the ideas in the expected order
+    doc = Nokogiri::HTML(last_response.body)
+    titles = doc.css('.ideas .title').map(&:text)
+    assert_equal ['a', 'c', 'b'], titles
+  end
+
+  def test_index_has_a_form_to_create_new_ideas
+    skip
+  end
+
+  def test_when_i_submit_the_form_it_creates_an_idea
+    skip
   end
 end
